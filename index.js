5f5403eead7d99e82249230575958b57f3b9abe2
OlmerOnline <olmeronline@mail.ru>
2025-05-14 18:28:36 +0400

    Добавил рендеринг комментариев и добавил событие клик на иконки лайк

--- index.js
+++ index.js
@@ -56,40 +56,102 @@ function removeClassError(input) {
     input.classList.remove('input-error');
 }
 
+const comments = [
+    {
+        name: "Глеб Фокин",
+        date: "12.02.22 12:18",
+        text: "Это будет первый комментарий на этой странице",
+        countLike: 3,
+        isActiveLike: false,
+    },
+    {
+        name: "Варвара Н.",
+        date: "13.02.22 19:22",
+        text: "Мне нравится как оформлена эта страница! ❤",
+        countLike: 75,
+        isActiveLike: true,
+    },
+];
+
 let listComment = document.querySelector('.comments');
 let btnWrite = document.querySelector('.add-form-button');
 let inputName = document.querySelector('.add-form-name');
 let inputText = document.querySelector('.add-form-text');
 
+function addEventBtnLike() {
+    const arrayBtnLike = document.querySelectorAll('.like-button');
+    for (const btnLike of arrayBtnLike) {
+        btnLike.addEventListener('click', () => {
+            if (comments[btnLike.dataset.index].isActiveLike) {
+                comments[btnLike.dataset.index].isActiveLike = false;
+                comments[btnLike.dataset.index].countLike--;
+            } else {
+                comments[btnLike.dataset.index].isActiveLike = true;
+                comments[btnLike.dataset.index].countLike++;            
+            }
+
+            renderComments()
+        });
+    }
+}
+
+function renderComments() {
+    const htmlComments = comments.map((comment, index) => {
+        return `
+        <li class="comment">
+          <div class="comment-header">
+            <div>${comment.name}</div>
+            <div>${comment.date}</div>
+          </div>
+          <div class="comment-body">
+            <div class="comment-text">
+              ${comment.text}
+            </div>
+          </div>
+          <div class="comment-footer">
+            <div class="likes">
+              <span class="likes-counter">${comment.countLike}</span>
+              <button data-index="${index}" class="like-button ${comment.isActiveLike ? '-active-like' : ''}"></button>
+            </div>
+          </div>
+        </li>`;
+    })
+    .join('');
+
+    listComment.innerHTML = htmlComments;
+
+    addEventBtnLike();
+}
+
+renderComments();
+
 btnWrite.addEventListener('click', () => {
-    console.log('click');
-
     if (inputName.value === '' || inputText.value === '') {
-    if (inputName.value === '') {
-        inputName.classList.add('input-error')
+        if (inputName.value === '') {
+            inputName.classList.add('input-error')
 
-        setTimeout(() => removeClassError(inputName), 2000);
-    }
-    if (inputText.value === '') {
-        inputText.classList.add('input-error')
+            setTimeout(() => removeClassError(inputName), 2000);
+        }
+        if (inputText.value === '') {
+            inputText.classList.add('input-error')
 
-        setTimeout(() => removeClassError(inputText), 2000);
-    }
+            setTimeout(() => removeClassError(inputText), 2000);
+        }
     } else {
-    let newComment = document.createElement('li');
-    newComment.className = 'comment';
+        let newComment = document.createElement('li');
+        newComment.className = 'comment';
 
-    let header = createCommentHeader(inputName.value);
-    let body = createCommentBody(inputText.value);
-    let footer = createCommentFooter();
+        let header = createCommentHeader(inputName.value);
+        let body = createCommentBody(inputText.value);
+        let footer = createCommentFooter();
 
-    newComment.appendChild(header);
-    newComment.appendChild(body);
-    newComment.appendChild(footer);
+        newComment.appendChild(header);
+        newComment.appendChild(body);
+        newComment.appendChild(footer);
 
-    listComment.appendChild(newComment);
+        listComment.appendChild(newComment);
 
-    inputName.value = '';
-    inputText.value = '';
+        inputName.value = '';
+        inputText.value = '';
     }
 });
